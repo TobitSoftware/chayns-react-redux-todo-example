c2920350a4bc75999b428551682cb05a2ba8edbf
Kai Schniggendiller <ki@tobit.software>
2019-02-21 14:03:56 +0100

    :art: Add constant LOCALSTORAGE_KEY

--- src/api/get/todos.js
+++ src/api/get/todos.js
@@ -1,7 +1,9 @@
+import LOCALSTORAGE_KEY from '../../constants/localstorage';
+
 // It's useful to define several functions for data interactions like get, post, update ... and split them in different directories
 async function getTodosFromLocalStorage() {
     return new Promise((resolve) => {
-        const todos = localStorage.getItem('todos');
+        const todos = localStorage.getItem(LOCALSTORAGE_KEY);
         if (todos) {
             resolve(JSON.parse(todos));
         } else {
--- src/api/post/todos.js
+++ src/api/post/todos.js
@@ -1,4 +1,6 @@
+import LOCALSTORAGE_KEY from '../../constants/localstorage';
+
 // It's useful to define several functions for data interactions like get, post, update ... and split them in different directories
 export default function postTodosToLocalStorage(todos) {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(todos));
 }
